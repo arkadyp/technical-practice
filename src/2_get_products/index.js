ed3cb15726635ffcf9ea0247fa63b95176ec3a1c
Arkady Pevzner <arkady@famo.us>
2015-07-09 21:16:20 -0700

    add second pass of product_at_every_index

--- src/2_get_products/index.js
+++ src/2_get_products/index.js
@@ -22,35 +22,58 @@
 // part 2, counting up from after the index
 // cycle array 3x, once to record upwards, once downwards, once to fill
 
+/*-----------------------------------------------------------------------------------------*/
+// First pass
+/*-----------------------------------------------------------------------------------------*/
+// function product_at_every_index(arr) {
+//     var lastIndex = arr.length - 1;
+//     var rightProduct = [arr[0]];
+//     var leftProduct  = [];
+//     leftProduct[lastIndex] = arr[lastIndex];
+//     var result = [];
+
+//     // calculate before and after arrays
+//     for (let i = 1, j = lastIndex - 1; i < arr.length && j >= 0; i++, j--) {
+//         rightProduct[i] = rightProduct[i - 1] * arr[i];
+//         leftProduct[j] = leftProduct[j + 1] * arr[j];
+//     }
+
+//     var behind;
+//     var ahead;
+//     for (let i = 0; i < arr.length; i++) {
+//         behind = rightProduct[i - 1] === undefined ? 1 : rightProduct[i - 1] ;
+//         ahead = leftProduct[i + 1] === undefined ? 1 : leftProduct[i + 1];
+//         result.push(behind * ahead);
+//     }
+
+//     return result;
+// }
+/*-----------------------------------------------------------------------------------------*/
+// Second pass
+/*-----------------------------------------------------------------------------------------*/
 function product_at_every_index(arr) {
-    var lastIndex = arr.length - 1;
-    var rightProduct = [arr[0]];
-    var leftProduct  = [];
-    leftProduct[lastIndex] = arr[lastIndex];
-    var result = [];
-
-    // calculate before and after arrays
-    for (let i = 1, j = lastIndex - 1; i < arr.length && j >= 0; i++, j--) {
-        rightProduct[i] = rightProduct[i - 1] * arr[i];
-        leftProduct[j] = leftProduct[j + 1] * arr[j];
-    }
-
-    var behind;
-    var ahead;
+    // Calculate products of all indicies before given number
+    var product_not_including_index = [];
+    var product = 1;
     for (let i = 0; i < arr.length; i++) {
-        behind = rightProduct[i - 1] === undefined ? 1 : rightProduct[i - 1] ;
-        ahead = leftProduct[i + 1] === undefined ? 1 : leftProduct[i + 1];
-        result.push(behind * ahead);
+        product_not_including_index.push(product);
+        product *= arr[i];
     }
 
-    return result;
+    product = 1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        product_not_including_index[i] *= product;
+        product *= arr[i];
+    }
+
+    return product_not_including_index;
 }
 
 var result;
 result = product_at_every_index([1, 2, 3, 4, 5]);
-console.log(result);
+console.log(result); // [120, 60, 40, 30, 24]
 
 result = product_at_every_index([1, 7, 3, 4]);
-console.log(result);
+console.log(result); // [84, 12, 28, 21]
 
 
