e394cdbe91fac7bdc6bd46e7b69c718b79eec60c
Arkady Pevzner <arkady@famo.us>
2015-07-14 11:42:07 -0700

    complete problem 6: find rectangle

---
+++ src/6_find_rectangle/index.js
@@ -0,0 +1,88 @@
+// A crack team of love scientists from OkEros (a hot new dating site) 
+// have devised a way to represent dating profiles as rectangles on a 
+// two-dimensional plane.
+
+// They need help writing an algorithm to find the intersection of two 
+// users' love rectangles. They suspect finding that intersection is 
+// the key to a matching algorithm so powerful it will cause an immediate 
+// acquisition by Google or Facebook or Obama or something.
+
+// Write a function to find the rectangular intersection of two 
+// given love rectangles.
+
+// As with the example above, love rectangles are always "straight" 
+// and never "diagonal." More rigorously: each side is parallel with 
+// either the x-axis or the y-axis.
+
+// They are defined as hash maps ↴ like this:
+
+//   my_rectangle = {
+
+//     # coordinates of bottom-left corner:
+//     'x': 1,
+//     'y': 5,
+
+//     # width and height
+//     'width': 10,
+//     'height': 4,
+
+// }
+// Your output rectangle should use this format as well.
+
+function find_rectangle(_rect1, _rect2) {
+    var rect1 = {
+        bottom_left: [_rect1.x, _rect1.y],
+        top_right: [_rect1.x + _rect1.width, _rect1.y + _rect1.height]
+    };
+
+    var rect2 = {
+        bottom_left: [_rect2.x, _rect2.y],
+        top_right: [_rect2.x + _rect2.width, _rect2.y + _rect2.height]
+    };
+
+    var left_rect = (rect1.bottom_left[0] <= rect2.bottom_left[0]) ? rect1 : rect2;
+    var right_rect = (rect1.top_right[0] >= rect2.top_right[1]) ? rect1 : rect2;
+    var bottom_rect = (rect1.bottom_left[1] <= rect2.bottom_left[1]) ? rect1: rect2;
+    var top_rect = (rect1.top_right[1] >= rect2.top_right[1]) ? rect1 : rect2;
+
+    // one rectangle is inside the other rectangle
+    if (left_rect === right_rect && bottom_rect === top_rect) {
+        var contained_rect = left_rect === rect1 ? rect2 : rect1;
+        return {
+            x: contained_rect.bottom_left[0],
+            y: contained_rect.bottom_left[1],
+            width: contained_rect.top_right[0] - contained_rect.bottom_left[0],
+            height: contained_rect.top_right[1] - contained_rect.bottom_left[1]
+        }
+    }
+
+
+    // Check if rectangles don't intersect
+        // either right_rect is to the right of leftmost left_rect
+        // OR
+        // bottom rect top point is below the bottom point of top rect  
+    if (
+        left_rect.top_right[0] < right_rect.bottom_left[0] || 
+        bottom_rect.top_right[1] < top_rect.bottom_left[1]
+    ) {
+        return null;
+    }
+    else {
+        var left_x = right_rect.bottom_left[0];
+        var right_x = left_rect.top_right[0];
+
+        var top_y = bottom_rect.top_right[1];
+        var bottom_y = top_rect.bottom_left[1];
+
+        return {
+            x: left_x,
+            y: bottom_y,
+            width: right_x - left_x,
+            height: top_y - bottom_y
+        };
+    }
+
+    return {};
+}
+
+module.exports = find_rectangle;
---
+++ test/problem_06.js
@@ -0,0 +1,78 @@
+var expect = require("chai").expect;
+var assert = require("chai").assert;
+var find_rectangle = require('../es5/6_find_rectangle');
+
+describe("Problem 6: find rectangle", function(){
+    it('should find intersection', function(){
+        var rectangle1 = {
+            x: 0,
+            y: 0,
+            height: 5,
+            width: 5
+        };
+
+        var rectangle2 = {
+            x: 2,
+            y: 2,
+            height: 5,
+            width: 5
+        };
+
+        var intersection_rectangle = find_rectangle(rectangle1, rectangle2);
+        var intersection_rectangle2 = find_rectangle(rectangle2, rectangle1);
+        var expected_intersection = {
+            x: 2,
+            y: 2,
+            height: 3,
+            width: 3
+        }
+
+        expect(intersection_rectangle).to.deep.equal(expected_intersection);
+        expect(intersection_rectangle2).to.deep.equal(expected_intersection);
+    });
+
+    it('should find intersection when one rectangle is inside of the other', function(){
+        var rectangle1 = {
+            x: 0,
+            y: 0,
+            height: 10,
+            width: 10
+        };
+
+        var rectangle2 = {
+            x: 2,
+            y: 2,
+            height: 5,
+            width: 5
+        };
+
+        var intersection_rectangle = find_rectangle(rectangle1, rectangle2);
+        var expected_intersection = {
+            x: 2,
+            y: 2,
+            height: 5,
+            width: 5
+        }
+
+        expect(intersection_rectangle).to.deep.equal(expected_intersection);
+    });
+
+    it('should return null if there isn\`t an intersection', function(){
+        var rectangle1 = {
+            x: 0,
+            y: 0,
+            height: 5,
+            width: 5
+        };
+
+        var rectangle2 = {
+            x: 10,
+            y: 10,
+            height: 5,
+            width: 5
+        };
+
+        var intersection_rectangle = find_rectangle(rectangle1, rectangle2);
+        expect(intersection_rectangle).to.deep.equal(null);
+    });
+});
