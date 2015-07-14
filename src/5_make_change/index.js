eb4b0251882ba8b08aecf52b89957e40e2315c30
Arkady Pevzner <arkady@famo.us>
2015-07-13 18:25:40 -0700

    practice make change

--- src/5_make_change/index.js
+++ src/5_make_change/index.js
@@ -1,29 +1,57 @@
-function make_change(total, _denom) {
-    // sort denominations in ascending order
-    var denominations = _denom.slice().sort((a, b) => a < b);
+function get_acsending_array(arr) {
+    return arr.slice(0).sort((a, b) => a - b);
+}
 
-    var count = 0;
-    function subroutine(running_total, index) {
-        if (running_total === total) {
-            count++;
-            return;
-        }
-        else if (running_total > total) {
-            return;
-        }
+function make_change_iterative(total, _denom) {
+    var denominations = get_acsending_array(_denom);
+    var possibilities = [1]; // base case --> one way to make 0
 
-        for (let i = index; i < denominations.length; i++) {
-            let value = denominations[i];
-            subroutine(running_total + value, i);
+    for (let i = 0; i < denominations.length; i++) {
+        let denomination = denominations[i];
+        for (let amount = 1; amount <= total; amount++) {
+            possibilities[amount] = possibilities[amount] || 0;
+            
+            if (amount >= denomination) {
+                possibilities[amount] += possibilities[amount - denomination];
+            }
         }
     }
 
-    subroutine(0, 0);
-    return count;
+    return possibilities[total];
 }
 
-module.exports = make_change;
+function make_change_recursive(total, _denom) {
+    var denominations = get_acsending_array(_denom);
+    var memo = {};
 
-// try to recursive, memoized
+    var count_possibilities = function(amount_left, index) {
+        var key = JSON.stringify([amount_left, index]);
+        if (memo[key] !== undefined) {
+            return memo[key];
+        }
 
-// try the bottom up approach
+        if (amount_left === 0) {
+            return 1;
+        }
+        else if (amount_left < 0) {
+            return 0;
+        }
+        if (index > denominations.length - 1) {
+            return 0;
+        }
+
+        var possibilities = 0;
+        var denomination = denominations[index];
+        while (amount_left >= 0) {
+            possibilities += count_possibilities(amount_left, index + 1);
+            amount_left -= denomination;
+        }
+        memo[key] = possibilities;
+        return possibilities;
+    }
+
+    return count_possibilities(total, 0);
+}
+
+
+module.exports = make_change_recursive;
--- test/problem_05.js
+++ test/problem_05.js
@@ -2,20 +2,44 @@ var expect = require("chai").expect;
 var assert = require("chai").assert;
 var make_change = require('../es5/5_make_change');
 
+// http://rosettacode.org/wiki/Count_the_coins#Iterative
+function countcoins(t, o) {
+    'use strict';
+    var targetsLength = t + 1;
+    var operandsLength = o.length;
+    t = [1];
+ 
+    for (var a = 0; a < operandsLength; a ++) {
+        for (var b = 1; b < targetsLength; b ++) {
+ 
+            // initialise undefined target
+            t[b] = t[b] ? t[b] : 0;
+ 
+            // accumulate target + operand ways
+            t[b] += (b < o[a]) ? 0 : t[b - o[a]];
+        }
+    }
+ 
+    return t[targetsLength - 1];
+}
+
 describe("Problem 5: make change", function() {
     it('should work with 4, [1,2,3]', function() {
         expect(make_change(4, [1,2,3])).to.equal(4);
+
     });
 
     it('should work with 100, [1, 5, 10, 25, 50, 100]', function() {
         expect(make_change(100, [1, 5, 10, 25, 50, 100])).to.equal(293);
+        expect(make_change(100, [1, 5, 10, 25, 50, 100])).to.equal(countcoins(100, [1, 5, 10, 25, 50, 100]));
     });
 
-    // it('should work with 200, [1, 5, 10, 25, 50]', function() {
-    //     expect(make_change(200, [1, 5, 10, 25, 50])).to.equal(2728);
-    // });
+    it('should work with 200, [1, 5, 10, 25, 50, 100]', function() {
+        expect(make_change(200, [1, 5, 10, 25, 50, 100])).to.equal(2728);
+        expect(make_change(200, [1, 5, 10, 25, 50, 100])).to.equal(countcoins(200, [1, 5, 10, 25, 50, 100]));
+    });
 
-    // it('should work with 2000, [1, 5, 10, 25, 50]', function() {
-    //     expect(make_change(2000, [1, 5, 10, 25, 50])).to.equal(155848897);
-    // });
+    it('should work with 2000, [1, 5, 10, 25, 50, 100]', function() {
+        expect(make_change(2000, [1, 5, 10, 25, 50, 100])).to.equal(countcoins(2000, [1, 5, 10, 25, 50, 100]));
+    });
 });
